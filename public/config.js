aba671c8b655b4246da9bdde4423d42f2dad0d76
Microyu <gxywy@hotmail.com>
2021-04-13 20:07:49 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,10 +2,10 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: "Microyu's Monitor",
 
   // 站点链接
-  SiteUrl: '/',
+  SiteUrl: 'https://status.microyu.top',
 
   // UptimeRobot Api 域名
   // 只需填写域名部分，默认为官网域名
@@ -16,10 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1163695-e4ff70df00f799edb813fa05',
   ],
 
   // 是否显示监测站点的链接
